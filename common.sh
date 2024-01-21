ec09e040964d6dad0292027af3c6106768b7f55e
sungyehong <sungyehong@gmail.com>
2024-01-22 08:41:30 +0900

    Add files via upload

---
+++ common.sh
@@ -0,0 +1,339 @@
+
+# Text Color
+RED="\033[0;31m"
+LRED="\033[1;31m"
+GREEN="\033[0;32m"
+BLUE="\033[0;34m"
+YELLOW="\033[0;33m"
+CYAN="\033[0;36m"
+LGRAY="\033[0;37m"
+DGRAY="\033[0;30m"
+NORMAL="\033[0m"
+
+# SSH Connection timeout
+SSH_TIMEOUT=3
+
+# 용량 출력시 소수점 이하 자리 수(0=>소수점이하 절삭, 2=>1자리...)
+DECIMALPOINT=1
+
+TB=1000000000000
+GB=1000000000
+MB=1000000
+KB=1000
+
+# 용량 값을 문자열로 변환
+# 소수점이하 자리수는 decimal로 조정(0=>소수점이하 절삭, 2=>1자리...) 
+# Parameter:
+#   $1: number
+#   $2: decimal point (default: 1)
+number2str() {
+    # number와 decimal이 없으면
+    number=${1:-0}
+    decimal=${2:-1}
+    
+    if [[ ${number} -ge ${TB} ]]; then 
+        tostr=$(echo "scale=${decimal}; ${number}/${TB}" | bc)
+        unit_str="T"    
+    elif [[ ${number} -ge ${GB} ]]; then
+        tostr=$(echo "scale=${decimal}; ${number}/${GB}" | bc)
+        unit_str="G"
+    elif [[ ${number} -ge ${MB} ]]; then
+        tostr=$(echo "scale=${decimal}; ${number}/${MB}" | bc)
+        unit_str="M"
+    elif [[ ${number} -ge ${KB} ]]; then
+        tostr=$(echo "scale=${decimal}; ${number}/${KB}" | bc)
+        unit_str="K"
+    else 
+        tostr=${number}
+        unit_str=""
+    fi 
+    # 소수점 이하가 .0이면 소수점 이하 삭제 1.0 => 1
+    tostr=$(echo ${tostr} | sed 's/.0$//')"${unit_str}"
+
+    echo ${tostr}
+}
+
+# 가로선 출력
+# Parameter:
+#  $1: 구분선 문자
+#  $2: 구분선 길이
+#  $3: 패딩 (optional)
+BD() {
+   # 좌측 패딩 
+   [[ ${3} -gt 0 ]] && printf "%-${3}s" " "
+
+   printf "+"
+   for i in $(seq 1 $((${2} - 2))); do
+      printf ${1}
+   done
+   printf "+"; printf "\n"
+}
+
+# 컬럼 출력 형식 설정
+# 한번 설정한 형식 지정은 형식 지정을 새로 하지 않으면 유지
+# Parameter:
+#  $@: 형식 지정 
+SETTD() {
+   while [[ ${#} -gt 0 ]]; do
+      case ${1} in
+         -a|--align)
+            shift 1
+            if [[ ! -z ${1} ]]; then 
+               TDALIGN=${1}
+            fi
+            shift 1
+            ;;
+         -w|--width)
+            shift 1
+            if [[ ! -z ${1} ]]; then
+               TDWIDTH=${1}
+            fi 
+            shift 1
+            ;;
+         -p|--pad)
+            shift 1
+            if [[ ! -z ${1} ]]; then
+               TDPAD=${1}
+            fi
+            ;;
+         *)
+            shift 1
+            ;;
+      esac
+   done
+}
+
+# 테이블 컬럼 출력
+# Parameter:
+#  $1: 컬럼 문자열
+#  $@: 형식 지정 (-a: 정렬, -w: 컬럼 넓이)
+TD() {
+   # 파라미터가 없으면 줄바꿈
+   if [[ ${#} -eq 0 ]]; then
+      printf "\n"
+      return 0
+   fi 
+
+   TDSTR="${1}"
+   shift 1; SETTD "${@}"
+
+   # 컬럼 문자열 정렬을 지정하지 않으면 가운데 정렬 
+   [[ -z ${TDALIGN} ]] && TDALIGN="C"
+
+   # 컬럼 넓이를 지정하지 않으면 문자열 크기
+   [[ -z ${TDWIDTH} ]] && TDWIDTH=${#TDSTR}
+
+   # 문자열이 지정 크기로 조정
+   TDSTR=${TDSTR::${TDWIDTH}}
+
+   [[ ${TDPAD} -gt 0 ]] && printf "%-${TDPAD}s" " "
+
+   case ${TDALIGN} in
+      L)
+         fmt="%-${TDWIDTH}s"
+         ;;
+      R)
+         fmt="%${TDWIDTH}s"
+         ;;
+      *)
+         # 가운데 정렬이고 지정 크기보다 문자 길이가 작으면
+         if [[ ${TDWIDTH} -gt $((${#TDSTR} + 1)) ]]; then 
+            # 좌우 마진 
+            m=$(((${TDWIDTH}-${#TDSTR})/2))
+            TDSTR=$(printf "%${m}s%${#TDSTR}s%${m}s" " " "${TDSTR}" " ")
+         fi
+         fmt="%${TDWIDTH}s"
+         ;;
+   esac 
+
+   printf ${fmt} "${TDSTR}"
+
+}
+
+# 컬럼 출력 형식 설정
+# Parameter:
+#   $@: 형식 지정 
+SETTR() {
+   while [[ ${#} -gt 0 ]]; do
+      case ${1} in
+         -w|--width)
+            shift 1
+            if [[ ! -z ${1} ]]; then 
+               TRWIDTH=$(( ${1} - 1 ))
+               shift 1
+            fi 
+            ;;
+         -a|--align)
+            shift 1
+            if [[ ! -z ${1} ]]; then 
+               TRALIGN=${1}
+               shift 1
+            fi
+            ;;
+         -l|--lbar)
+            shift 1
+            if [[ ! -z ${1} &&  ${1} != -* ]]; then 
+               TRLBAR=${1}
+               shift 1
+            else 
+               TRLBAR=" "
+            fi 
+            ;;
+         -c|--cbar)
+            shift 1
+            if [[ ! -z ${1} &&  ${1} != -* ]]; then 
+               TRCBAR=${1}
+               shift 1
+            else 
+               TRCBAR=" "
+            fi 
+            ;;
+         -r|--rbar)
+            shift 1
+            if [[ ! -z ${1} &&  ${1} != -* ]]; then 
+               TRRBAR=${1}
+               shift 1
+            else 
+               TRRBAR=" "
+            fi 
+            ;;
+         -p|--padding)
+            shift 1
+            if [[ ! -z ${1} ]]; then 
+               TRPAD=${1}
+               shift 1
+            fi
+            ;;
+         *)
+            shift 1
+            ;;
+      esac
+   done 
+}
+
+# 테이블 행 출력 형식 설정
+# Parameter:
+#  $1: 행 문자열 (; 구분, ;의 앞뒤 여백도 컬럼에 포함됨)
+#  $@: 출력 옵션  
+# 사용 예:
+#  TR "%s -w 20  "
+#  TR "%s -l |"
+#  TR "HELLO;12345";TR
+#  TR "%s -w 20 -l | -a C;HELLO;12345;%s -w10;HELLO;12345"; TR
+TR() {
+   # 파라미터 없이 호출하면 줄바꿈 
+   if [[ -z ${1} ]]; then 
+      TD; return 0
+   fi 
+
+   # 테이블내 컬럼 문자열에 스페이스가 포함되어 있을 수 있어 ; 문자로 구분
+   oldifs=$IFS; IFS=";"
+   # 컬럼 문자열을 어레이형으로
+   TRSTR=($(echo "${1}"))
+   IFS=${oldifs}
+
+   # 패딩 설정이 있으면, 패딩 사용은 사전 선언 필요
+   # TR "%s -p 5"; TR "Hello"
+   [[ ${TRPAD} -gt 0 ]] && TD " " -w ${TRPAD}
+   # 컬럼 출력
+   for i in ${!TRSTR[@]}; do
+      # 포멧 설정
+      if [[ ${TRSTR[${i}]:0:2} == "%s" ]]; then 
+         SETTR ${TRSTR[${i}]:2:${#TRSTR[${i}]}}
+      else 
+         # width가 선언되지 않으면 컬럼 문자열 길이
+         [[ -z ${TRWIDTH} ]] && TRWIDTH=${#TRSTR[${i}]}
+         [[ -z ${TRALIGN} ]] && TRALIGN=C
+         
+         # 첫번째 컬럼 시작
+         if [[ ${i} -eq 0 ]]; then 
+            [[ ! -z ${TRLBAR} ]] && TD "${TRLBAR}" -w 1 || TD " " -w 1
+         fi
+
+         # 마지막 컬럼이면
+         if [[ ${i} -eq $(( ${#TRSTR[@]} - 1)) ]]; then 
+            TD "${TRSTR[${i}]}" -w $(( ${TRWIDTH} - 1 )) -a ${TRALIGN}
+            [[ ! -z ${TRRBAR} ]] &&  TD "${TRRBAR}" -w 1 || TD " " -w 1
+         else 
+            TD "${TRSTR[${i}]}" -w ${TRWIDTH} -a ${TRALIGN}
+            [[ ! -z ${TRCBAR} ]] && TD "${TRCBAR}" -w 1 || TD " " -w 1
+         fi 
+      fi
+   done
+}
+
+# 문자열 리스트를  홀수는 좌측 짝수는 우축으로 정렬
+# Parameter:
+#  $1: 문자열 ,로 구분 
+relocate_str_by_name() {
+    odd_list=""
+    even_list=""
+
+    # 소팅하고, 어레이 형으로 변환
+    list=($(echo ${1} | tr "," " " | sort | tr "\n" " "))
+
+    for name in ${list[@]}; do
+        # 마지막 문자만 추출
+        last_number=$(echo ${name} | awk '{print substr($0, length,1)}')
+        last_number=$((${last_number} % 2))
+        
+        if [[ ${last_number} -eq 0 ]]; then 
+            even_list+="${name},"
+        else 
+            odd_list+="${name},"
+        fi 
+    done 
+
+    # 풀 리스트 합치고 마지막 , 문자 제거후 반환
+    echo $(echo "${odd_list}${even_list}" | sed 's/.$//')
+}
+
+# SSL 연결 확인
+# Parameter:
+#   $1: IP Address
+#   $2: User ID
+validate_connection() {
+    if [[ -z ${1} || -z ${2} ]]; then 
+        echo "usage: ${0} <ip address> <user id>"
+        return 1
+    else  
+        # 연결 시도
+        ssh -q -o ConnectTimeout=${SSH_TIMEOUT} ${2}@${1} exit
+        rtn_code=${?}
+        
+        if [[ ${rtn_code} -ne 0 ]]; then 
+            echo "${LRED}Connect to host ${1} timed out${NORMAL}"
+        fi
+
+        return ${rtn_code} 
+    fi
+}
+
+# 리무트 서버에 SQL 실행
+# Parameter:
+#   $1: IP Address
+#   $2: User ID
+#   $3: SQL Statement
+#   $4: sqlplus login string (optional, Default: / as sysdba)
+runsql() {
+    # loging string를 지정하지 않으면
+    login_str=${4:-/ as sysdba}
+
+    ret_var=$(ssh -o ConnectTimeout=${SSH_TIMEOUT} ${2}@${1} sqlplus -s ${login_str} <<EOF
+WHENEVER OSERROR EXIT 5;
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
+${3}
+EXIT;
+EOF
+)
+    rtn_code=${?}
+	if [[ ${rtn_code} -eq 5 ]] ; then
+		SQLRUNERROR="sqlplus 실행중 OS 에러 발생 했습니다. ${ret_var}"
+		unset ret_var
+	elif [[ $rtn_code -ne 0 ]] ; then
+		SQLRUNERROR="sqlplus 실행중 SQL 에러 발생 했습니다. ${ret_var}"
+		unset ret_var
+	fi
+    
+    echo "${ret_var}"
+}
---
+++ zbrmusage.sh
@@ -0,0 +1,566 @@
+#!/bin/sh
+#│▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒│
+# 
+# ZFS Appliance Usage v0.7
+# 
+# zbrmusage.sh [-if]
+# - zfsadashboard.sh -i 5 -f inventory.ini       ZFSA Resource 상태 5초 간격으로 보기
+#			
+#│▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒│
+
+# Default inventory 파일
+INVENTORY_FILE=./inventory.ini
+
+# 업데이트 대기 시간 (0: 1회 수행, n: n초 간격으로 반복)
+INTERVAL=0
+# 업데이트 시간 최소값 
+MIN_INTERVAL=5
+
+# Dictionary 데이터 구조 생성
+if [[ $SHELL == "/bin/ksh" ]]; then 
+    typeset -A ZFSA_INVENTORY
+    typeset -A ORADB_INVENTORY
+    typeset -A ZFSA_USAGE
+    typeset -A ORADB_USAGE
+else 
+    declare -A ZFSA_INVENTORY
+    declare -A ORADB_INVENTORY
+    declare -A ZFSA_USAGE
+    declare -A ORADB_USAGE
+fi
+
+# ZFS Appliance 현황 정보를 딕셔너리 구조의 문자열로 리턴
+# Parameter:
+#   $1: IP address
+#   $2: User ID
+#   $3: ZFSA Name 
+get_zfsa_usage() {
+
+    zfsa_script="
+    script
+    {
+        const ZFSA_CTL='${3}';
+        // 용량 단위 환산을 위한 상수
+        const TB = 1000000000000;
+        const GB = 1000000000;
+        const MB = 1000000;
+        const KB = 1000;
+        // 용량 출력시 소수점 이하 자리 수(0=>소수점이하 절삭, 2=>1자리...)
+        const DECIMAL_POINT = 2; 
+
+        // 사용 현황 객체 변수
+        // var ZFSA_USAGE = {};
+
+        // 풀, 프로젝트 어레이 변수
+        var zfsa_pools = [];
+        var zfsa_projects = [];
+        var zfsa_pool_projects = [];
+
+        // 용량 문자열을 바이트 정수로 변환
+        function change2byte(data) {
+            if (data[data.length - 1] == 'K') {
+                data = data.slice(0, data.length - 1) * KB;
+            }
+            else if (data[data.length - 1] == 'M') {
+                data = data.slice(0, data.length - 1) * MB;
+            }
+            else if (data[data.length - 1] == 'G') {
+                data = data.slice(0, data.length - 1) * GB;
+            }
+            else if (data[data.length - 1] == 'T') {
+                data = data.slice(0, data.length - 1) * TB;
+            }
+
+            return data;
+        }
+
+        // 용량 값을 문자열로 변환
+        // 소수점이하 자리수는 decimal로 조정(0=>소수점이하 절삭, 2=>1자리...) 
+        function number2str(num, decimal) {
+            // 데이터가 없으면 ' ' 값 리턴
+            if (num == undefined) {
+                return ' ';
+            }
+
+            if (num >= TB) {
+                tostr = parseFloat(num / TB).toString();
+                tostr = tostr.slice(0, tostr.indexOf('.')+decimal) + 'TB';
+            }
+            else if (num >= GB) {
+                tostr = parseFloat(num / GB).toString();
+                tostr = tostr.slice(0, tostr.indexOf('.')+decimal) + 'GB';
+            }
+            else if (num >= MB) {
+                tostr = parseFloat(num / MB).toString();
+                tostr = tostr.slice(0, tostr.indexOf('.')+decimal) + 'MB';
+            }
+            else if (num >= KB) {
+                tostr = parseFloat(num / KB).toString();
+                tostr = tostr.slice(0, tostr.indexOf('.')+decimal) + 'KB';
+            }
+            else {
+                tostr = num + 'B';
+            }
+            return tostr;
+        }
+
+        // 어레이 항목 중복제거
+        function remove_duplicates(arr) {
+            unique = [];
+            
+            for (i = 0; i < arr.length; i++) {
+                if (unique.indexOf(arr[i]) === -1) {
+                    unique.push(arr[i]);
+                }
+            }
+            return unique;
+        }
+
+        // Hardware info
+        function get_hardware() {
+            run('cd /');
+            run('maintenance');
+            run('hardware');
+
+            hwlist = list();
+            for(hw=0; hw<hwlist.length; hw++) {
+                print(hwlist[hw]);
+            }
+            
+            // list -> select chassis matched name -> select disk
+            // zfsa_system_state= state = ok, or ??
+            // ZFSA_USAGE[]
+            // zfsa_disk_state= no of disks, ok or ??
+            // hardware_disks = ''; 
+        }
+
+        // ZFSA Controller 전체 풀 리스트 딕셔너리 문자열로 출력
+        // 전체 풀: ZFSA_USAGE=[ZFSACTL.pools]
+        function get_pool() {
+            run('cd /');
+            run('status');
+            run('storage');
+
+            zfsa_pools = list();
+
+            ret_str = 'ZFSA_USAGE['+ZFSA_CTL+'.pools]=\"';
+            ret_str = ret_str + zfsa_pools.toString();
+            ret_str = ret_str + '\"';
+
+            fmt = '%-'+ret_str.length+'s';
+            // 쉘 스크립에서 출력 문자열을 환경 변수로 변경해 사용
+            printf(fmt, ret_str); print('');
+        } 
+
+        // ZFSA Controller 전체 프로젝트와 풀 프로젝트 딕셔너리 생성
+        // 전체 프로젝트: ZFSA_USAGE=[ZFSACTL.projects]
+        // 풀 프로젝트: ZFSA_USAGE=[ZFSACTL.pool.projects]
+        function get_project() {
+            run('cd /');
+            run('shares');
+           
+            // 전체 풀 리스트
+            // zfsa_pools = choices('pool').sort();
+
+            // default를 제외한 프로젝트 리스트
+            for (i=0; i<zfsa_pools.length; i++) {
+                pool_name = zfsa_pools[i];
+
+                set('pool', pool_name);
+                zfsa_pool_projects = list().sort();
+
+                // default project 제외
+                zfsa_pool_projects.splice(zfsa_pool_projects.indexOf('default'));
+
+                ret_str = 'ZFSA_USAGE['+ZFSA_CTL+'.'+pool_name+'.projects]=\"';
+                ret_str = ret_str + zfsa_pool_projects.toString();
+                ret_str = ret_str + '\"';
+
+                // 풀 프로젝트 리스트를 출력
+                fmt = '%-'+ret_str.length+'s';
+                printf(fmt, ret_str); print('');
+
+                // Pool의 프로젝트 항목 합치고
+                zfsa_projects = zfsa_projects.concat(zfsa_pool_projects);
+            }
+
+            // 중복된 프로젝트 항목 제거하고
+            zfsa_projects = remove_duplicates(zfsa_projects);
+
+            ret_str = 'ZFSA_USAGE['+ZFSA_CTL+'.projects]=\"';
+            ret_str = ret_str + zfsa_projects.toString();
+            ret_str = ret_str + '\"';
+
+            // 전체 프로젝트 리스트를 출력
+            fmt = '%-'+ret_str.length+'s';
+            printf(fmt, ret_str); print('');
+        }
+
+        // ZFSA CPU, IO. NFS, NIC activity report
+        function get_activity() {
+            run('cd /');
+            run('status');
+            run('activity');
+
+            // Activity 객체 생성
+            zfsa_activity = {};
+
+            // CPU Utilization(%)
+            run('select cpu.utilization');
+            zfsa_activity.cpu = parseInt(run('get average').split(/\s+/)[3]);
+            run('cd ..');
+
+            // I/O operation(op/sec)
+            run('select io.ops');
+            zfsa_activity.io = parseInt(run('get average').split(/\s+/)[3]);
+            run('cd ..');
+
+            // NFS operation(op/sec)
+            run('select nfs3.ops');
+            zfsa_activity.nfs = parseInt(run('get average').split(/\s+/)[3]);
+            run('cd ..');
+
+            run('select nfs4.ops');
+            zfsa_activity.nfs += parseInt(run('get average').split(/\s+/)[3]);
+            run('cd ..');
+            
+            // Network bandwidth
+            run('select nic.kilobytes')
+            zfsa_activity.nic = parseInt(run('get average').split(/\s+/)[3]);
+
+            // Resource activity를 dictionary 구조의 문자열로 출력
+            ret_str = 'ZFSA_USAGE['+ZFSA_CTL+'.activity.cpu]='+zfsa_activity.cpu;
+            fmt = '%-'+ret_str.length+'s';
+            printf(fmt, ret_str); print('');
+
+            ret_str = 'ZFSA_USAGE['+ZFSA_CTL+'.activity.io]='+zfsa_activity.io;
+            fmt = '%-'+ret_str.length+'s';
+            printf(fmt, ret_str); print('');
+
+            ret_str = 'ZFSA_USAGE['+ZFSA_CTL+'.activity.nfs]='+zfsa_activity.nfs;
+            fmt = '%-'+ret_str.length+'s';
+            printf(fmt, ret_str); print('');
+
+            ret_str = 'ZFSA_USAGE['+ZFSA_CTL+'.activity.nic]='+zfsa_activity.nic;
+            fmt = '%-'+ret_str.length+'s';
+            printf(fmt, ret_str); print('');
+        }
+
+        // ZFSA 스토리지 풀 사용 현황 
+        // used, avail, total 용량을 바이트 단위로 출력
+        function get_usage_by_pool(pool_name) {
+            run('cd /');
+            run('status');
+            run('storage');
+            run('select ' + pool_name);
+
+            used = change2byte(run('get used').split(/\s+/)[3]);
+            avail = change2byte(run('get avail').split(/\s+/)[3]);
+            total = used + avail;
+
+            // Pool usage를 dictionary 구조의 문자열로 출력
+            ret_str = 'ZFSA_USAGE['+ZFSA_CTL+'.'+pool_name+'.used]='+parseFloat(used).toString();
+            fmt = '%-'+ret_str.length+'s';
+            printf(fmt, ret_str); print('');
+
+            ret_str = 'ZFSA_USAGE['+ZFSA_CTL+'.'+pool_name+'.avail]='+parseFloat(avail).toString();
+            fmt = '%-'+ret_str.length+'s';
+            printf(fmt, ret_str); print('');
+
+            ret_str = 'ZFSA_USAGE['+ZFSA_CTL+'.'+pool_name+'.total]='+parseFloat(total).toString();
+            fmt = '%-'+ret_str.length+'s';
+            printf(fmt, ret_str); print('');
+        } 
+
+        // ZFSA 스토리지 프로젝트 사용 현황
+        function get_usage_by_project(pool_name, project_name) {
+            run('cd /');
+            run('shares');
+            set('pool', pool_name);
+            pool_projects = list();
+
+            // Project가 pool에 존재하면 바이트로 변환후 할당
+            if (pool_projects.indexOf(project_name) != -1) {
+                run('select ' + project_name);
+
+                used_data = change2byte(run('get space_data').split(/\s+/)[3]);
+                used_snapshot = change2byte(run('get space_snapshots').split(/\s+/)[3]);
+                used_total = change2byte(run('get space_total').split(/\s+/)[3]); 
+
+                run('cd ..');
+            } else { // Project가 pool에 존재하지 않으면 
+                used_data = -1;
+                used_snapshot = -1;
+                used_total = -1; 
+            }
+
+            // Project usage를 dictionary 구조의 문자열로 출력
+            ret_str = 'ZFSA_USAGE['+ZFSA_CTL+'.'+pool_name+'.'+project_name+'.used_data]='+used_data.toString();
+            fmt = '%-'+ret_str.length+'s';
+            printf(fmt, ret_str); print('');
+
+            ret_str = 'ZFSA_USAGE['+ZFSA_CTL+'.'+pool_name+'.'+project_name+'.used_snapshot]='+used_snapshot.toString();
+            fmt = '%-'+ret_str.length+'s';
+            printf(fmt, ret_str); print('');
+
+            ret_str = 'ZFSA_USAGE['+ZFSA_CTL+'.'+pool_name+'.'+project_name+'.'+'used_total]='+used_total.toString();
+            fmt = '%-'+ret_str.length+'s';
+            printf(fmt, ret_str); print('');
+        } 
+
+        // Main 
+        try {
+            // get_hardware();
+            get_pool();
+            get_project();
+            get_activity();
+
+            for (i=0; i<zfsa_pools.length; i++) {
+                get_usage_by_pool(zfsa_pools[i]);
+            }
+
+            for (i=0; i<zfsa_pools.length; i++) {
+                for (j=0; j<zfsa_projects.length; j++) { 
+                    get_usage_by_project(zfsa_pools[i], zfsa_projects[j]);
+                }
+            }
+        } catch (err) {
+            print(err);
+        }
+    }"
+    
+    ret_val=$(echo "$zfsa_script" | ssh -T -o ConnectTimeout=${SSH_TIMEOUT} "${2}@${1}" | grep -v Last)
+
+    echo "$ret_val"
+}
+
+#  __  _   _          _                                               
+#   / |_) |_) |\/|   |_)  _   _  _      ._ _  _    | |  _  _.  _   _  
+#  /_ |_) | \ |  |   | \ (/_ _> (_) |_| | (_ (/_   |_| _> (_| (_| (/_ 
+#                                                              _|     
+show_zfsa_usage_by_project() {
+    # 전체 풀과 프로젝트 리스트
+    for zfsa in ${!ZFSA_INVENTORY[@]}; do
+        all_pools+="${ZFSA_USAGE[${zfsa}.pools]},"
+        all_projects+="${ZFSA_USAGE[${zfsa}.projects]},"
+    done 
+
+    # pool 이름으로 홀/짝으로 좌우 정렬하고 어레이로 생성
+    all_pools=($(relocate_str_by_name "${all_pools}" | tr "," " "))
+    # 프로젝트 정렬, 중복 제거
+    all_projects=($(echo ${all_projects} | tr "," "\n" | sort -u | tr "\n" " "))
+
+    # 테이블 표시 형식 정의
+    tr_width_usage_col=6
+    tr_width_pool_col=$(( tr_width_usage_col * 3 ))
+    tbl_row_width=$(( ${tr_width_pool_col} + ${#all_pools[@]} * ${tr_width_pool_col} + ${tr_width_pool_col} ))
+        
+    BD "=" ${tbl_row_width}
+    TR "%s -a L -w ${tbl_row_width} -l | -c | -r |"
+
+    for zfsa in ${!ZFSA_INVENTORY[@]}; do
+        TR " ${zfsa}"; TR
+        TR "    Pool: ${ZFSA_USAGE[${zfsa}.pools]}"; TR
+        TR "    CPU: ${ZFSA_USAGE[${zfsa}.activity.cpu]}% \
+                I/O: ${ZFSA_USAGE[${zfsa}.activity.io]} IOPS \
+                NFS: ${ZFSA_USAGE[${zfsa}.activity.nfs]} OPS \
+                NET: ${ZFSA_USAGE[${zfsa}.activity.nic]} KB"; TR
+    done    
+    BD "=" ${tbl_row_width}
+
+    # 
+    BD "=" ${tbl_row_width}
+    for pool_name in ${all_pools[@]}; do
+        pool_str+="${pool_name};"
+        pool_str1+=" ;"
+        pool_str2+="DATA;SNAP;TOTAL;"
+    done 
+    # 마지막 ; 문자 제거
+    pool_str=$(echo "${pool_str}" | sed 's/.$//')
+    pool_str1=$(echo "${pool_str1}" | sed 's/.$//')
+    pool_str2=$(echo "${pool_str2}" | sed 's/.$//')
+
+    # 타이틀 행
+    TR "%s -a C -w ${tr_width_pool_col} -l | -c | -r |"
+    TR " ;${pool_str}; "; TR 
+    TR " ;${pool_str1}; "; TR 
+    TR "PROJECTS;%s -w ${tr_width_usage_col};${pool_str2};%s -w ${tr_width_pool_col};SUM"; TR
+    BD "-" ${tbl_row_width}
+    
+    for project_name in ${all_projects[@]}; do
+        project_used_sum=0
+        project_used_str="${project_name};"
+
+        for pool_name in ${all_pools[@]}; do 
+            for zfsa in ${!ZFSA_INVENTORY[@]}; do
+                zfsa_pools="${ZFSA_USAGE[${zfsa}.pools]}"
+                if [[ ${zfsa_pools} =~ ${pool_name} ]]; then 
+                    used_data=${ZFSA_USAGE[${zfsa}.${pool_name}.${project_name}.used_data]}
+                    used_snapshot=${ZFSA_USAGE[${zfsa}.${pool_name}.${project_name}.used_snapshot]}
+                    used_total=${ZFSA_USAGE[${zfsa}.${pool_name}.${project_name}.used_total]}
+                    
+                    if [[ ${used_total} -ne -1 ]]; then
+                        project_used_sum=$((${project_used_sum} + ${used_total}))
+                        used_data=$(number2str ${used_data})
+                        used_snapshot=$(number2str ${used_snapshot})
+                        used_total=$(number2str ${used_total})
+
+                        project_used_str+="%s -w ${tr_width_usage_col};${used_data};${used_snapshot};${used_total};"
+                    else 
+                        project_used_str+="%s -w ${tr_width_usage_col};;;;"
+                    fi 
+                fi 
+            done
+        done 
+
+        project_used_sum=$(number2str ${project_used_sum})
+        project_used_str+="%s -w ${tr_width_pool_col};${project_used_sum}"
+        TR "${project_used_str}"; TR 
+    done
+    BD "=" ${tbl_row_width}
+
+    # Pool별 사용 현황
+    pool_total_str="Total;"; pool_used_str="Used;"; pool_avail_str="Avail;"; pool_avail_ratio_str=";"
+    pool_total_sum=0; pool_used_sum=0; pool_avail_sum=0
+
+    for pool_name in ${all_pools[@]}; do 
+        for zfsa in ${!ZFSA_INVENTORY[@]}; do
+            zfsa_pools="${ZFSA_USAGE[${zfsa}.pools]}"
+            if [[ ${zfsa_pools} =~ ${pool_name} ]]; then 
+                pool_total=${ZFSA_USAGE[${zfsa}.${pool_name}.total]}
+                pool_used=${ZFSA_USAGE[${zfsa}.${pool_name}.used]}
+                pool_avail=${ZFSA_USAGE[${zfsa}.${pool_name}.avail]}
+
+                pool_total_sum=$(( ${pool_total_sum} + ${pool_total} ))
+                pool_used_sum=$(( ${pool_used_sum} + ${pool_used} ))
+                pool_avail_sum=$(( ${pool_avail_sum} + ${pool_avail} ))
+
+                pool_total_str+="$(number2str ${pool_total});"
+                pool_used_str+="$(number2str ${pool_used});"
+                pool_avail_str+="$(number2str ${pool_avail});"
+                pool_avail_ratio_str+="[$(( ${pool_avail} * 100 / ${pool_total} ))%];"
+            fi 
+        done
+    done 
+
+    pool_total_str+="$(number2str ${pool_total_sum})"
+    pool_used_str+="$(number2str ${pool_used_sum})"
+    pool_avail_str+="$(number2str ${pool_avail_sum})"
+    pool_avail_ratio_str+="[$(( ${pool_avail_sum} * 100 / ${pool_total_sum} ))%]"
+
+    TR ${pool_total_str}; TR
+    TR ${pool_used_str}; TR
+    TR ${pool_avail_str}; TR
+    TR ${pool_avail_ratio_str}; TR
+    BD "=" ${tbl_row_width}
+}
+
+# config 파일에서 ZFSA와 DB서버 정보 확인
+parsing_config_file() {
+
+    while IFS=" " read -r name serverip userid option
+    do
+        # 코멘트, 빈줄 제거 
+        if [[ ${name} =~ ^# || ${name} =~ ^\; || -z ${name} ]]; then                                 # Ignore comments / empty lines
+            continue;
+        fi
+        
+        # ZFSA 섹션이면
+        if [[ ${name} =~ ^"["ZFSA"]"$ ]]; then
+            section=ZFSA
+        # ORADB 섹션이면
+        elif [[ ${name} =~ ^"["ORADB"]"$ ]]; then
+            section=ORADB 
+        else 
+            eval "${section}_INVENTORY[${name}]=${serverip}:${userid}:${option}"
+        fi 
+    done < ${INVENTORY_FILE}
+}
+
+# Arguments 파싱
+parsing_arg() {
+    while [[ ${#} -gt 0 ]]; do
+        case ${1} in
+            -i|--interval)
+                shift 1
+                if [[ -z ${1} || ! ${1} =~ ^[0-9]+$ ]]; then
+                    help
+                else
+                    INTERVAL=${1}
+                    UPDATE_MODE="ON"
+                fi
+
+                shift 1
+                ;;
+            -f|--file)
+                shift 1
+                if [[ -z ${1} ]]; then
+                    help
+                else
+                    INVENTORY_FILE="${1}"
+                    shift 1
+                fi
+                ;;
+        esac
+    done
+}
+
+# inventory 파일 해석, 데이터 초기화
+init() {
+
+    # Common function 로딩
+    . ./common.sh
+
+    # 수행 옵션 파싱
+    parsing_arg 
+
+    # 인벤토리 파일이 없으면 
+    if [[ -z ${INVENTORY_FILE} || ! -f ${INVENTORY_FILE} ]]; then
+        echo -e ${RED}"Inventory file not found."${NORMAL}
+        exit 1
+    fi 
+
+    # Inventory 파일 파싱후 ZFSA_INVENTORY, ORADB_INVENTORY dictionary 생성
+    parsing_config_file ${INVENTORY_FILE}
+
+    # 인벤토리 항목이 없으면
+    if [[ ${#ZFSA_INVENTORY[@]} -eq 0 && ${#ORADB_INVENTORY[@]} ]]; then 
+        echo -e ${RED}"Inventory file contains no items."${NORMAL}
+        exit 1
+    fi 
+
+    # 모든 ZFSA Controller에 접속해 현황 정보를 딕셔너리 구조로 저장 
+    for key in "${!ZFSA_INVENTORY[@]}"; do
+        para=$(echo ${ZFSA_INVENTORY[$key]} | tr ":" " ") 
+        
+        # [테스트]현황 정보를 파일로 저장할때
+        # get_zfsa_usage ${para} ${key} > zfsa.out
+
+        # [실사용]현황 정보의 딕셔너리 구조를 쉘 환경 변수로 
+        # eval "$(get_zfsa_usage ${para} ${key})"
+    done
+    # [테스트] 저장된 파일의 딕셔너릴 구조를 쉘 환경 변수로 
+    eval "$(cat zfsa.out)"
+}
+
+# Main rountie
+# 초기화
+init 
+
+while true :
+do
+    clear
+    show_zfsa_usage_by_project
+
+    if [[ ${INTERVAL} -eq 0 ]]; then
+        break
+    fi
+
+    sleep ${INTERVAL}
+done 
+
+exit 0
+
+#│▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒│
+# End of script
+#│▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒│
